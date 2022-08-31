96a0fb4ea48fd0cec705201d3440e2627bc22310
Shubham <iamshubham316@gmail.com>
2022-08-31 15:50:06 +0530

    commit v1

--- js/forms.js
+++ js/forms.js
@@ -6,6 +6,44 @@ function hi(){
 	document.getElementById("demo").innerHTML= "Hello!";
 }
 
+
+/*window.onload = () => {
+        // create a couple of elements in an otherwise empty HTML page
+        const heading2 = document.createElement("h2");
+        const headingText2 = document.createTextNode("Big Head 2!");
+        heading2.appendChild(headingText2);
+        document.body.appendChild(heading2);
+        
+        
+        const heading = document.createElement("h1");
+        const headingText = document.createTextNode("Big Head!");
+        heading.appendChild(headingText);
+        document.body.appendChild(heading);
+       
+      };*/
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+
+//main-stuff starts here
+
+
 function profileName(){
 	const username= document.getElementById("demo");
 	const profname= username;
